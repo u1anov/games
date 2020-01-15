964397718d1bfa2e9c2d08e423469a8cc0239b01
u1anov <59798390+u1anov@users.noreply.github.com>
2020-01-15 22:34:43 +0300

    исправлен bgCycle

--- Runner/javascript/index.js
+++ Runner/javascript/index.js
@@ -9,6 +9,7 @@ var camera = pjs.camera;
 var size = vector.size;
 var point = vector.point;
 var game = pjs.game;
+var presets = pjs.presets;
 
 var touch = pjs.touchControl;
 touch.initTouchControl();
@@ -19,14 +20,6 @@ var width = game.getWH().w;
 var fonBG = game.newImageObject({
     x:0, y:0,
     file:'img/e57a57608b61a8d90fa3388041ea55ec.jpg',
-    h:height,
-    onload:function(){
-       fonBGTwo.x = fonBG.w+fonBG.x;
-    }
-});
-var fonBGTwo = game.newImageObject({
-    x:fonBG.w+fonBG.x, y:0,
-    file:'img/e57a57608b61a8d90fa3388041ea55ec.jpg',
     h:height
 });
 var ground = game.newImageObject({
@@ -34,14 +27,8 @@ var ground = game.newImageObject({
     file:'img/sozdanie-beskonechnogo-rannera-na-JavaScript-mehanika-dvijeniya-fona-3.png',
     w:width,
     onload:function(){
-      groundTwo.y = ground.y = height - ground.h;
-      groundTwo.x = ground.w + ground.x;
+      ground.y = height - ground.h;
     }
-});
-var groundTwo = game.newImageObject({
-    x:0, y:0,
-    file:'img/sozdanie-beskonechnogo-rannera-na-JavaScript-mehanika-dvijeniya-fona-3.png',
-    w:width,
 });
 var dog = game.newAnimationObject({
     x:0, y:0,
@@ -49,32 +36,13 @@ var dog = game.newAnimationObject({
     delay:5,
     animation: pjs.tiles.newAnimation('img/sozdanie-beskonechnogo-rannera-na-JavaScript-mehanika-dvijeniya-fona-4.png',150,180,5)
 });
-var moveBackGround = function (s){
-    fonBG.move(point(-s / 2, 0));
-    fonBGTwo.move(point(-s / 2, 0));
-
-    ground.move(point(-s, 0));
-    groundTwo.move(point(-s, 0));
-
-    if(fonBG.x + fonBG.w < 0 ){
-        fonBG.x = fonBGTwo.x + fonBG.w;
-    };
-    if(fonBGTwo.x + fonBGTwo.w < 0 ){
-        fonBGTwo.x = fonBG.x + fonBG.w;
-    }
-    if(ground.x + ground.w < 0 ){
-        ground.x = groundTwo.x + ground.w;
-    };
-    if(groundTwo.x + groundTwo.w < 0 ){
-        groundTwo.x = ground.x + ground.w;
-    }
-};
 game.newLoop('Runner',function(){
+
+    presets.bgCycle(fonBG, -5, 0, 0, 0);
     fonBG.draw();
-    fonBGTwo.draw();
+    presets.bgCycle(ground, -5, 0, 0, 0);
     ground.draw();
-    groundTwo.draw();
-    moveBackGround(5);
+    
     dog.y = ground.y - dog.h / 3
     dog.draw();
 });
